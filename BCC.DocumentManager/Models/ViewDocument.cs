a7f3eddd01d2febb03e5d7f83f98f0b0944290c4
shontykov <shontykov.kz@gmail.com>
2020-02-07 17:54:12 +0600

    added swagger and cleaned up

--- BCC.DocumentManager/Models/ViewDocument.cs
+++ BCC.DocumentManager/Models/ViewDocument.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BCC.DocumentManager.Models
+﻿namespace BCC.DocumentManager.Models
 {
     public class ViewDocument
     {
